a92949a4433f627bacd4f9c1e67f68f3640e7a45
kasiagrygorowicz <kasiagrygorowicz@interia.eu>
2022-05-07 15:09:10 +0200

    ....

--- src/main/java/world/Camera.java
+++ src/main/java/world/Camera.java
@@ -1,14 +1,20 @@
 package world;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.swing.*;
 import java.awt.*;
-import java.util.Set;
+import java.util.List;
+
 
 public class Camera extends JPanel {
 
-//  == camera parameters ==
+    private static Logger log = LoggerFactory.getLogger(Camera.class);
+
+    //  == camera parameters ==
     private final int width = 12;
-    private final int height =12;
+    private final int height = 12;
     private double zoom = 2;
 
     //  == world position ==
@@ -21,13 +27,13 @@ public class Camera extends JPanel {
     private double rotationAxisY = 0.0;
     private double rotationAxisZ = 0.0;
 
-    private Set<Line> objects = null;
+    private List<Line> objects = null;
 
-    public Camera(){
+    public Camera() {
         this.setBackground(Color.BLACK);
     }
 
-    public void setObjects(Set<Line> objects){
+    public void setObjects(List<Line> objects) {
         this.objects = objects;
         this.positionAxisX = 0.0;
         this.positionAxisY = 0.0;
@@ -37,76 +43,76 @@ public class Camera extends JPanel {
         this.rotationAxisZ = 0.0;
     }
 
-//  == move camera ==
-    public void moveAxisX(double v){
+    //  == move camera ==
+    public void moveAxisX(double v) {
         System.out.println(positionAxisX);
         positionAxisX += v;
     }
 
-    public void moveAxisY(double v){
+    public void moveAxisY(double v) {
 //        System.out.println("moving y");
         positionAxisY += v;
     }
 
-//  == zoom ==
-    public void setZoom(double v){
+    //  == zoom ==
+    public void setZoom(double v) {
 
-        zoom +=v;
+        zoom += v;
         System.out.println(zoom);
     }
 
-    public void moveAxisZ(double v){
+    public void moveAxisZ(double v) {
         positionAxisZ += v;
     }
 
-//  == rotate camera ==
-    public void rotateAxisX(double v){
+    //  == rotate camera ==
+    public void rotateAxisX(double v) {
         rotationAxisX += v;
     }
 
-    public void rotateAxisY(double v){
+    public void rotateAxisY(double v) {
         rotationAxisY += v;
     }
 
-    public void rotateAxisZ(double v){
+    public void rotateAxisZ(double v) {
         rotationAxisZ += v;
     }
 
-//  == transform coordinates of line ==
-    private Line transform(Line line){
+    //  == transform coordinates of line ==
+    private Line transform(Line line) {
 
 //        point 1
-        double x1 = line.getP1().getX() +positionAxisX;
-        double y1 = line.getP1().getY()+positionAxisY;
-        double z1 = line.getP1().getZ()+positionAxisZ;
+        double x1 = line.getP1().getX() + positionAxisX;
+        double y1 = line.getP1().getY() + positionAxisY;
+        double z1 = line.getP1().getZ() + positionAxisZ;
 
 //        point 2
-        double x2 = line.getP2().getX()+positionAxisX;
-        double y2 = line.getP2().getY()+positionAxisY;
-        double z2 = line.getP2().getZ()+positionAxisZ;
+        double x2 = line.getP2().getX() + positionAxisX;
+        double y2 = line.getP2().getY() + positionAxisY;
+        double z2 = line.getP2().getZ() + positionAxisZ;
 
 //        rotate x axis
-        double radiusX = (rotationAxisX *Math.PI) /180;
-        y1 =y1*Math.cos(radiusX) - z1*Math.sin(radiusX);
-        z1 = y1*Math.sin(radiusX) + z1*Math.cos(radiusX);
-        y2 = y2*Math.cos(radiusX) - z2*Math.sin(radiusX);
-        z2 = y2*Math.cos(radiusX) + z2*Math.cos(radiusX);
+        double radiusX = (rotationAxisX * Math.PI) / 180;
+        y1 = y1 * Math.cos(radiusX) - z1 * Math.sin(radiusX);
+        z1 = y1 * Math.sin(radiusX) + z1 * Math.cos(radiusX);
+        y2 = y2 * Math.cos(radiusX) - z2 * Math.sin(radiusX);
+        z2 = y2 * Math.cos(radiusX) + z2 * Math.cos(radiusX);
 
 //        rotate y axis
-        double radiusY = (rotationAxisY *Math.PI) /180;
-        x1 = z1*Math.sin(radiusY) + x1*Math.cos(radiusY);
-        z1 = z1*Math.cos(radiusY) - x1*Math.sin(radiusY);
-        x2 = z2*Math.sin(radiusY) + x2*Math.cos(radiusY);
-        z2 = z2*Math.cos(radiusY) - x2*Math.sin(radiusY);
+        double radiusY = (rotationAxisY * Math.PI) / 180;
+        x1 = z1 * Math.sin(radiusY) + x1 * Math.cos(radiusY);
+        z1 = z1 * Math.cos(radiusY) - x1 * Math.sin(radiusY);
+        x2 = z2 * Math.sin(radiusY) + x2 * Math.cos(radiusY);
+        z2 = z2 * Math.cos(radiusY) - x2 * Math.sin(radiusY);
 
 //        rotate z axis
-        double radiusZ = (rotationAxisZ *Math.PI) /180;
-        x1 = x1*Math.cos(radiusZ) - y1*Math.sin(radiusZ);
-        y1 = x1*Math.sin(radiusZ) + y1*Math.cos(radiusZ);
-        x2 = x2*Math.cos(radiusZ) - y2*Math.sin(radiusZ);
-        y2 = x2*Math.sin(radiusZ) + y2*Math.cos(radiusZ);
+        double radiusZ = (rotationAxisZ * Math.PI) / 180;
+        x1 = x1 * Math.cos(radiusZ) - y1 * Math.sin(radiusZ);
+        y1 = x1 * Math.sin(radiusZ) + y1 * Math.cos(radiusZ);
+        x2 = x2 * Math.cos(radiusZ) - y2 * Math.sin(radiusZ);
+        y2 = x2 * Math.sin(radiusZ) + y2 * Math.cos(radiusZ);
 
-        return new Line(new Point(x1,y1,z1),new Point(x2,y2,z2));
+        return new Line(new Point(x1, y1, z1), new Point(x2, y2, z2));
     }
 
     @Override
@@ -116,37 +122,39 @@ public class Camera extends JPanel {
 //        System.out.println("painting");
 
 
-        if(objects!=null){
+        if (objects != null) {
 //            System.out.println("in if");
-            for(Line line : objects){
+            for (Line line : objects) {
 //                System.out.println("in the loop");
                 Line transformed = transform(line);
 //                System.out.println(line.toString());
 //todo ?
-                if(transformed.getP1().getZ()>0 || transformed.getP2().getZ() >0){
+                if (transformed.getP1().getZ() > 0 || transformed.getP2().getZ() > 0) {
                     double x1, y1, x2, y2;
                     int xScaled, yScaled, xPositioned, yPositioned;
 
-                    x1 = transformed.getP1().getX()* zoom /(transformed.getP1().getZ()+ zoom);
-                    y1 = -transformed.getP1().getY()* zoom /(transformed.getP1().getZ()+ zoom);
-                    x2 = transformed.getP2().getX()* zoom /(transformed.getP2().getZ()+ zoom);
-                    y2 = -transformed.getP2().getY()* zoom /(transformed.getP2().getZ()+ zoom);
+                    x1 = transformed.getP1().getX() * zoom / (transformed.getP1().getZ() + zoom);
+                    y1 = -transformed.getP1().getY() * zoom / (transformed.getP1().getZ() + zoom);
+                    x2 = transformed.getP2().getX() * zoom / (transformed.getP2().getZ() + zoom);
+                    y2 = -transformed.getP2().getY() * zoom / (transformed.getP2().getZ() + zoom);
 
 //                  == scale ==
-                    xScaled =getSize().width/width;
-                    yScaled = getSize().height/height;
+                    xScaled = getSize().width / width;
+                    yScaled = getSize().height / height;
 
-                    xPositioned =getSize().width/2;
-                    yPositioned =getSize().height/2;
+                    xPositioned = getSize().width / 2;
+                    yPositioned = getSize().height / 2;
+
+                    log.debug("\tx1: {}\ty1: {}\tx2: {}\ty2: {}", (int) Math.ceil(x1 * xScaled + xPositioned), (int) Math.ceil(y1 * yScaled + yPositioned), (int) Math.ceil(x2 * xScaled + xPositioned), (int) Math.ceil(y2 * yScaled + yPositioned));
 
 
-                    g2D.setStroke(new BasicStroke(1));
+                    g2D.setStroke(new BasicStroke(2));
                     g2D.setColor(new Color(204, 204, 204));
                     g2D.drawLine(
-                            (int)Math.ceil(x1*xScaled+xPositioned),
-                            (int)Math.ceil(y1*yScaled+yPositioned),
-                            (int)Math.ceil(x2*xScaled+xPositioned),
-                            (int)Math.ceil(y2*yScaled+yPositioned)
+                            (int) Math.ceil(x1 * xScaled + xPositioned),
+                            (int) Math.ceil(y1 * yScaled + yPositioned),
+                            (int) Math.ceil(x2 * xScaled + xPositioned),
+                            (int) Math.ceil(y2 * yScaled + yPositioned)
                     );
                 }
             }
